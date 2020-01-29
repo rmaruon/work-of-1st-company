4be45accbbac96287f6c143a96b6af60812f4c85
rmaruon <22119585+rmaruon@users.noreply.github.com>
2020-01-30 00:06:40 +0900

    Add: data.js

---
+++ src/data.js
@@ -0,0 +1,41 @@
+export const data = {
+  directAndIndirectWorkChart: {
+    title: '直接作業と間接作業',
+    labels: ['直接作業', '間接作業'],
+    percentage: [74.7, 25.3],
+  },
+  directWorkChart: {
+    title: 'フェーズ',
+    labels: [
+      '要件定義',
+      '設計',
+      '実装',
+      'テスト',
+      'プロジェクト管理',
+      'アップグレード',
+      'インフラ構築',
+      '保守',
+    ],
+    percentage: [0.3, 27.9, 45.2, 4.9, 3.8, 7.3, 10.2, 0.4],
+  },
+  roleChart: {
+    title: '役割',
+    labels: ['リーダー', 'メンバー'],
+    percentage: [52.6, 47.4],
+  },
+  programmingLanguageChart: {
+    title: 'メイン開発言語',
+    labels: ['有償RADツール', 'Microsoft Office Access'],
+    percentage: [97.2, 2.8],
+  },
+  DBMSChart: {
+    title: 'DBMS',
+    labels: ['Oracle Database', 'Microsoft SQL Server', 'Microsoft Office Access', 'ほか'],
+    percentage: [67.3, 10.3, 2.8, 19.6],
+  },
+  indirectChart: {
+    title: '間接作業',
+    labels: ['教育指導', '教育受講', '社内SE', 'その他'],
+    percentage: [32.5, 27.6, 9.0, 30.9],
+  },
+};
